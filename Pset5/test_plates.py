f4f27c133c656365eab2b64b81327bd7f82a45ae
MUMMANA JAGADEESH <75290381+Mummanajagadeesh@users.noreply.github.com>
2024-06-06 11:13:50 +0530

    Create test_plates.py

---
+++ Pset5/test_plates.py
@@ -0,0 +1,18 @@
+from plates import is_valid
+
+def main():
+    test_plates()
+
+def test_plates():
+    assert is_valid("CS50")==True
+    assert is_valid("ECTO88")==True
+    assert is_valid("NRVOUS")==True
+    assert is_valid("CS05")==False
+    assert is_valid("50")==False
+    assert is_valid("CS50P2")==False
+    assert is_valid("PI3.14")==False
+    assert is_valid("H")==False
+    assert is_valid("OUTATIME")==False
+
+if __name__=="__main__":
+    main()
