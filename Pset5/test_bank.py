de3591b24460e7f396abc5f928c9a13cd4d2555d
MUMMANA JAGADEESH <75290381+Mummanajagadeesh@users.noreply.github.com>
2024-06-06 11:13:17 +0530

    Create test_bank.py

---
+++ Pset5/test_bank.py
@@ -0,0 +1,21 @@
+from bank import value
+
+def main():
+    test_bank()
+
+def test_bank():
+    assert value("Hello")==0
+
+    assert value(" Hello ")==0
+
+    assert value("Hello, Newman")==0
+
+    assert value("How are you doing?")==20
+
+    assert value("What's happening?")==100
+
+    assert value("What's up?")==100
+
+
+if __name__=="__main__":
+    main()
